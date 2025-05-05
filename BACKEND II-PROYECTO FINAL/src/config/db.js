63ecb28ab99ebf453fe99d2644edabf5b83a1ad3
Florencia Palacio <137527381+FlorenciaPalacio@users.noreply.github.com>
2025-05-05 10:55:10 -0300

    Update db.js

--- BACKEND II-PROYECTO FINAL/src/config/db.js
+++ BACKEND II-PROYECTO FINAL/src/config/db.js
@@ -1,1 +1,15 @@
+import mongoose from "mongoose";
+import config from "./config.js";
 
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(config.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("\u2705 Conectado a MongoDB correctamente");
+  } catch (error) {
+    console.error("\u274C Error al conectar a MongoDB:", error);
+    process.exit(1);
+  }
+};
